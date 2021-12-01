942338a9737cf0e465e99054969032b526e98fa2
SR0725 <62081739+SR0725@users.noreply.github.com>
2021-12-01 22:46:31 +0800

    updata3

--- 8-3.c
+++ 8-3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-  int input[30];    //宣告輸入數列
-  int i = 0;  //宣告判定輸入到第幾位數值得flag
-  int inputEnd = 0;  //宣告變數用來儲存使用者輸入字元長度
+  int input[30];      //宣告輸入數列
+  int i = 0;          //宣告判定輸入到第幾位數值得flag
+  int inputEnd = 0;   //宣告變數用來儲存使用者輸入字元長度
   printf("Enter a number: ");
   while(i < 30 && (input[i] = getchar()) != '\n'){  //如果i小於30的情況下且使用者未按下enter則將使用者每個輸入的字元存入input陣列中
     i++;
