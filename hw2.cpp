81b2358f67810548acbdc10f2b730848e9cec35a
SR0725 <62081739+SR0725@users.noreply.github.com>
2022-04-12 20:08:10 +0800

    Update hw2.cpp

--- hw2.cpp
+++ hw2.cpp
@@ -3,20 +3,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iomanip>
+#include <conio.h>
+#include <conio.h>
+#include <cstring>
 
 using namespace std;
 void myTable9x9();
 void myTable9x9(int);
 void myTable9x9(int, int);
 
-
 int main(){
-    std::cout << "myTable9x9():" << '\n';
-    myTable9x9();
-    std::cout << "myTable9x9(3):" << '\n';
-    myTable9x9(3);
-    std::cout << "myTable9x9(3,9):" << '\n';
-    myTable9x9(3,9);
+    string input;
+    int inputInt[10],inputIntLength = 0;
+    getline(cin, input);
+
+    for (int i = 0; i < input.length(); i++) {
+        if(input[i] > 47 && input[i] <= 57){
+            inputInt[inputIntLength] = input[i] - 48;
+            inputIntLength++;
+        }
+    }
+    if(inputIntLength == 0){
+        myTable9x9();
+    }else if(inputIntLength == 1){
+        myTable9x9(inputInt[0]);
+    }else{
+        myTable9x9(inputInt[0], inputInt[1]);
+    }
+
 }
 
 void myTable9x9(){
