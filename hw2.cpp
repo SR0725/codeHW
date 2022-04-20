c37a8e5aebb7c65281c93ca8c1a90e3b7ba45687
SR0725 <62081739+SR0725@users.noreply.github.com>
2022-04-20 16:19:46 +0800

    Update hw2.cpp

--- hw2.cpp
+++ hw2.cpp
@@ -1,61 +1,96 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <iomanip>
-#include <conio.h>
-#include <conio.h>
-#include <cstring>
 
+#include <iostream>
+#include "math.h"
 using namespace std;
-void myTable9x9();
-void myTable9x9(int);
-void myTable9x9(int, int);
+
+template <class T>
+class MyTriangle {
+private:
+    T a, b, c;
+public:
+    // 請為其添加適當的建構函式(constructor)。
+    MyTriangle(){
+      this->a = 1;
+      this->b = 1;
+      this->c = 1;
+    }
+    MyTriangle(T len){
+      this->a = len;
+      this->b = len;
+      this->c = len;
+    }
+    MyTriangle(T len1, T len2){
+        this->a = len1;
+        this->b = len1;
+        this->c = len2;
+
+
+      if( !isAvailable() ){
+        this->a = 0;
+        this->b = 0;
+        this->c = 0;
+      }
+    }
+    MyTriangle(T len1, T len2, T len3){
+        this->a = len1;
+        this->b = len2;
+        this->c = len3;
+
+      if( !isAvailable() ){
+        this->a = 0;
+        this->b = 0;
+        this->c = 0;
+      }
+    }
+
+    bool isAvailable() {
+        T maxLen = this->a;
+        T otherLens = this->b + this->c;
+        if(maxLen < this->b){
+            maxLen = this->b;
+            otherLens = this->a + this->c;
+        }
+        if(maxLen < this->c){
+            maxLen = this->c;
+            otherLens = this->a + this->b;
+        }
+        if(maxLen > otherLens){
+    		return false;
+        }else{
+    		return true;
+        }
+    }
+
+    float area() {
+        float s = (this->a +this->b+this->c)/2.0;
+	    return sqrt(s*(s - this->a)*(s - this->b)*(s - this->c));
+    }
+
+    float perimeter() {
+        return this->a+this->b+this->c;
+    }
+
+    void show() {
+    	cout << "三角形，邊長" << a << ", " << b << ", " << c << endl;
+    	cout << "area:" << area() << endl;
+    	cout << "perimeter:" << perimeter() << endl;
+    }
+
+    void showAll() {
+        cout << "Class: MyTriangle" << endl;
+        show();
+    }
+};
 
 int main(){
-    string input;
-    int inputInt[10],inputIntLength = 0;
-    getline(cin, input);
+    MyTriangle<int> triangle1;
+    MyTriangle<int> triangle2(2);
+    MyTriangle<int> triangle3(3,4);
+    MyTriangle<int> triangle4(5,6,7);
 
-    for (int i = 0; i < input.length(); i++) {
-        if(input[i] > 47 && input[i] <= 57){
-            inputInt[inputIntLength] = input[i] - 48;
-            inputIntLength++;
-        }
-    }
-    if(inputIntLength == 0){
-        myTable9x9();
-    }else if(inputIntLength == 1){
-        myTable9x9(inputInt[0]);
-    }else{
-        myTable9x9(inputInt[0], inputInt[1]);
-    }
+    triangle1.show();
+    triangle2.show();
+    triangle3.show();
+    triangle4.show();
 
-}
-
-void myTable9x9(){
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
-            cout << i << "x" << j << "=" << setw(2) << right << i*j << " ";
-        }
-        std::cout << '\n';
-    }
-}
-
-void myTable9x9(int row){
-    for (int i = row; i <= row; i++) {
-        for (int j = 1; j <= 9; j++) {
-            cout << i << "x" << j << "=" << setw(2) << right << i*j << " ";
-        }
-        std::cout << '\n';
-    }
-}
-
-void myTable9x9(int row_min, int row_max){
-    for (int i = row_min; i <= row_max; i++) {
-        for (int j = 1; j <= 9; j++) {
-            cout << i << "x" << j << "=" << setw(2) << right << i*j << " ";
-        }
-        std::cout << '\n';
-    }
 }
